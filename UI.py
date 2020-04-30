cce0da7a88e64d46c954a4544c91f1e7d61f8cf8
jianda1013 <jianda851013@gmail.com>
2020-04-30 09:26:59 +0800

    demo

--- UI.py
+++ UI.py
@@ -2,6 +2,7 @@
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 import cad
 
 hatch_list = ["ANGLE", "ANSI31", "ANSI32", "ANSI33", "ANSI34", "ANSI37",
@@ -9,110 +10,64 @@ hatch_list = ["ANGLE", "ANSI31", "ANSI32", "ANSI33", "ANSI34", "ANSI37",
 "CROSS","DOLMIT", "DOTS", "EARTH", "ESCHER", "FLEX", "GOST_GLASS", 
 "GOST_GROUND", "GOST_WOOD"]
 
-material_list = []
+material_list = ['灰鏡', '茶鏡', '門片', '上修門片']
+main_list = ['客餐廳', '主臥', '兒子房', '次臥室']
 
 def layer():
 
-	lst = cad.list_layer()
-	def send():
-		cad.switch(layer_selection.get())
+	def go(main, obj):
+		cad.switch(main)
+		cad.set_hatch(hatch_list[obj])
+		cad.hatch()
 		layer_frame.destroy()
-		hch()
+		comment(obj)
 
 	layer_frame = tk.Frame(window)
-	layer_frame.grid(column=0, row=0)
+	layer_frame.grid(column=0, row=0, padx=20, pady=10)
+	layer_frame.configure(background='white')
 
-	layer_text = tk.Label(layer_frame, text='選擇所取圖層')
-	layer_text.grid(column=0, row=0)
+	main_text = tk.Label(layer_frame, text='主體大項')
+	main_text.grid(column=0, row=0, padx=10, pady=10)
+	main_text.configure(background='white')
+	
+	main_select = ttk.Combobox(layer_frame, value=main_list, state='readonly', width=10)
+	main_select.grid(column=1, row=0, padx=10, pady=10)
+	main_select.current(0)
 
-	layer_selection = ttk.Combobox(layer_frame, values=lst, state='readonly')
-	layer_selection.grid(column=0, row=1)
-	layer_selection.current(0)
+	obj_text = tk.Label(layer_frame, text='細項品名')
+	obj_text.grid(column=0, row=1)
+	obj_text.configure(background='white')
 
-	layer_send = tk.Button(layer_frame, text='確認送出', command=send)
-	layer_send.grid(column=1, row=1)
+	obj_select = ttk.Combobox(layer_frame, value=material_list, state='readonly', width=10)
+	obj_select.grid(column=1, row=1)
+	obj_select.current(0)
 
-	layer_new = tk.Button(layer_frame, text='開新圖層', command=lambda: [layer_frame.destroy(), nlayer()])
-	layer_new.grid(column=0, row=2, padx=10, pady=0)
+	sendout = tk.Button(layer_frame, text='Go', command=lambda:[go(main_select.get(), obj_select.current())])
+	sendout.grid(column=2, row=1)
+	sendout.configure(background='white')
 
-	layer_exit = tk.Button(layer_frame, text='結束並儲存', command=lambda: cad.exit())
-	layer_exit.grid(column=1, row=3, padx=20, pady=10)
-
-def nlayer():
-
-	nlayer_frame = tk.Frame(window)
-	nlayer_frame.grid(column=0, row=0)
-
-	nlayer_text = tk.Label(nlayer_frame, text='新圖層名稱')
-	nlayer_text.grid(column=0, row=0)
-
-	nlayer_create = tk.Entry(nlayer_frame)
-	nlayer_create.grid(column=0, row=1)
-
-	nlayer_send = tk.Button(nlayer_frame, text='確認送出', command=lambda: [cad.switch(nlayer_create.get()), nlayer_frame.destroy(), hch()])
-	nlayer_send.grid(column=1, row=1)
-
-	nlayer_back = tk.Button(nlayer_frame, text='返回', command=lambda: [nlayer_frame.destroy(), layer()])
-	nlayer_back.grid(column=1, row=2)
-
-def hch():
-
-	def send(num):
-		cad.set_hatch(hatch_list[num])
-		cad.hatch()
-		hch_frame.destroy()
-		comment()
-
-	hch_frame = tk.Frame(window)
-	hch_frame.grid(column=0, row=0)
-
-	hch_text = tk.Label(hch_frame, text='選取材質')
-	hch_text.grid(column=0, row=0)
-
-	hch_select = ttk.Combobox(hch_frame, values=material_list, state='readonly')
-	hch_select.grid(column=0, row=1)
-	hch_select.current(0)
-
-	hch_send = tk.Button(hch_frame, text='確認送出', command=lambda: [send(hch_select.current())])
-	hch_send.grid(column=1, row=1)
-
-	hch_mtl = tk.Button(hch_frame, text='新增材質', command=lambda: [hch_frame.destroy(), mtl()])
-	hch_mtl.grid(column=0, row=2)
-
-	hch_back = tk.Button(hch_frame, text='返回', command=lambda: [hch_frame.destroy(), layer()])
-	hch_back.grid(column=1, row=2)
-
-def mtl():
-
-	def send(name):
-		material_list.append(name)
-		mtl_frame.destroy()
-		hch()
-
-	mtl_frame = tk.Frame(window)
-	mtl_frame.grid(column=0, row=0)
-
-	mtl_text = tk.Label(mtl_frame, text='輸入材質')
-	mtl_text.grid(column=0, row=0)
-
-	mtl_name = tk.Entry(mtl_frame)
-	mtl_name.grid(column=0, row=1)
-
-	mtl_send = tk.Button(mtl_frame, text='確認送出', command=lambda: [send(mtl_name.get())])
-	mtl_send.grid(column=1, row=1)
-
-def comment():
+def comment(num):
 	comment_frame = tk.Frame(window)
 	comment_frame.grid(column=0, row=0)
+	comment_frame.configure(background='white')
 
-	comment_text = tk.Button(comment_frame, text='確認完成邊界處理', command=lambda: [comment_frame.destroy(), cad.cal(), layer()])
+	comment_text = tk.Button(comment_frame, text='邊界處理完成', command=lambda: [comment_frame.destroy(), cad.cal(num), layer()])
 	comment_text.grid(column=0, row=0, padx=10, pady=10)
+	comment_text.configure(background='white')
 
 window = tk.Tk()
 window.title('CAD_UI')
-window.geometry('280x130')
+window.geometry('280x100+800+60')
 window.configure(background='white')
+window.resizable(False, False)
+
 
 layer()
 
+
+def on_closing():
+	cad.exit()
+	window.destroy()
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
 window.mainloop()
--- cad.py
+++ cad.py
@@ -9,12 +9,11 @@ print(acad.doc.Name)
 
 wb = Workbook()
 ws = wb.active
-ws['A1'] = '圖層'
-ws['B1'] = '材質'
+ws['A1'] = '主項大廳'
+ws['B1'] = '細項品名'
 ws['C1'] = '面積'
 
-def list_layer():
-	return [acad.ActiveDocument.Layers.Item(i).Name for i in range(acad.ActiveDocument.Layers.Count)]
+mat = ['灰鏡', '茶鏡', '門片', '上修門片']
 
 def switch(name):
 	acad.ActiveDocument.ActiveLayer = acad.ActiveDocument.Layers.Add(name)
@@ -25,17 +24,20 @@ def set_hatch(name):
 def hatch():
 	acad.ActiveDocument.Sendcommand("h"+chr(13))
 
-def cal():
+def cal(num):
 	for obj in acad.iter_objects("Hatch"):
 		if(obj.Layer == acad.ActiveDocument.ActiveLayer.Name):
-			ws.append([obj.Layer, obj.PatternName, obj.Area])
+			ws.append([obj.Layer, mat[num], obj.Area])
+			if(num == 2):
+				obj.erase()
 
 	for obj in acad.iter_objects("PolyLine"):
 		if(obj.Layer == acad.ActiveDocument.ActiveLayer.Name):
 			x = (obj.Coordinates[0] + obj.Coordinates[2] + obj.Coordinates[4] + obj.Coordinates[6])/4
 			y = (obj.Coordinates[1] + obj.Coordinates[3] + obj.Coordinates[5] + obj.Coordinates[7])/4
 			start = str(x)+','+str(y)
-			end = str(x)+','+str(y*2)
+			end = str(x)+',-4104'
 			acad.ActiveDocument.Sendcommand("mleader"+chr(13)+start+chr(13)+end+chr(13))
+			break
 def exit():
 	wb.save('info.xlsx')
